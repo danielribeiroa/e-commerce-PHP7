99b15b9ca2b89db09fbe4dccb41cd12b08e63ce0
Daniel <danielribeiro.a@outlook.com>
2020-09-03 14:12:41 -0300

    forgot your password added

--- vendor/projeto/php-classes/src/Model/User.php
+++ vendor/projeto/php-classes/src/Model/User.php
@@ -2,12 +2,16 @@
 
 namespace Projeto\Model;
 
+use Projeto\Mailer;
 use \Projeto\DB\Sql;
 use \Projeto\Model;
 
 class User extends Model{
 
     const SESSION = "User";
+    const SECRET = "senha_mt_segura";
+    const SECRET_IV = "outra_senha_segura";
+
     public static function login($login, $password)
     {
       $sql = new Sql();
@@ -141,5 +145,113 @@ class User extends Model{
                                                                     ":iduser"=>$this->getiduser()
                                                                 ));
     }
+    public static function getForgot($email, $inadmin = true)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+                            SELECT *
+                            FROM tb_persons a
+                            INNER JOIN tb_users b USING(idperson)
+                            WHERE a.desemail = :email;", array(":email"=>$email));
+
+		if (count($results) === 0)
+		{
+
+			throw new \Exception("Não foi possível recuperar a senha.");
+
+		}
+		else
+		{
+
+			$data = $results[0];
+
+			$results2 = $sql->select("CALL db_ecommerce.sp_userspasswordsrecoveries_create(
+                                                                                    :iduser, 
+                                                                                    :desip)", 
+                                                                                    array(":iduser"=>$data['iduser'],
+                                                                                        ":desip"=>$_SERVER['REMOTE_ADDR']));
+			if (count($results2) === 0)
+			{
+
+				throw new \Exception("Não foi possível recuperar a senha.");
+
+			}
+			else
+			{
+
+				$dataRecovery = $results2[0];
+
+				$code = openssl_encrypt($dataRecovery['idrecovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($code);
+
+				if ($inadmin === true) {
+
+					$link = "http://www.projetoecommerce.com.br/admin/forgot/reset?code=$code";
+
+				} else {
+
+					$link = "http://www.projetoecommerce.com.br/forgot/reset?code=$code";
+					
+				}				
+
+                $mailer = new Mailer($data['desemail'], 
+                                            $data['desperson'], 
+                                            "Redefinir senha", 
+                                            "forgot", array("name"=>$data['desperson'],
+					                                        "link"=>$link));				
+				$mailer->send();
+				return $link;
+			}
+		}
+    }
     
+	public static function validForgotDecrypt($code)
+	{
+
+		$code = base64_decode($code);
+
+		$idrecovery = openssl_decrypt($code, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+                            SELECT *
+                            FROM tb_userspasswordsrecoveries a
+                            INNER JOIN tb_users b USING(iduser)
+                            INNER JOIN tb_persons c USING(idperson)
+                            WHERE
+                            a.idrecovery = :idrecovery
+                            AND
+                            a.dtrecovery IS NULL
+                            AND
+                            DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();", array(":idrecovery"=>$idrecovery));
+		if (count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha.");
+		}
+		else
+		{
+			return $results[0];
+		}
+    }
+
+    public static function setForgotUsed($idrecovery)
+    {
+        $sql = new Sql();
+
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+			":idrecovery"=>$idrecovery
+		));
+    }
+    public function setPassword($password)
+    {
+        $sql = new Sql();
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+			":password"=>$password,
+			":iduser"=>$this->getiduser()
+	    ));
+    }
 }
