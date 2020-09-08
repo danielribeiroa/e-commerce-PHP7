a060e3599e83953fcc35ebf58b7847c8a0c9c45b
Daniel <danielribeiro.a@outlook.com>
2020-09-08 15:06:18 -0300

    products admin

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Projeto\PageAdmin;
 use \Slim\Slim;
 use \Projeto\Model\User;
 use \Projeto\Model\Category;
+use Projeto\Model\Product;
 
 $app = new Slim();
 
@@ -268,6 +269,57 @@ $app->get("/categories/:idcategory", function($idcategory){
    ]);
 });
 
+$app->get("/admin/products", function (){
+   User::verifyLogin();
+   $products = new Product();
+   $products = Product::listAll();
+   $page = new PageAdmin();
+   $page->setTpl("products", [
+      "products"=>$products
+   ]);
+});
+
+$app->get("/admin/products/create", function(){
+   User::verifyLogin();
+   $page = new PageAdmin();
+   $page->setTpl("products-create");
+});
+
+$app->post("/admin/products/create", function(){
+   User::verifyLogin();
+   $product = new Product();
+   $product->setData($_POST);
+   $product->save();
+   header("Location: /admin/products");
+   exit;
+});
+
+$app->get("/admin/products/:idproduct", function($idproduct){
+   User::verifyLogin();
+   $product = new Product();
+   $product->get((int)$idproduct);
+   $page = new PageAdmin();
+   $page->setTpl("products-update", ['product'=>$product->getValues()]);
+});
+
+$app->post("/admin/products/:idproduct", function($idproduct){
+   User::verifyLogin();
+   $product = new Product();
+   $product->get((int)$idproduct);
+   $product->setData($_POST);
+   $product->save();
+   $product->setPhoto($_FILES["file"]);
+   header("Location: /admin/products");
+   exit;
+});
+
+$app->get("/admin/products/:idproduct/delete", function($idproduct){
+   $product = new Product();
+   $product->get((int)$idproduct);
+   $product->delete();
+   header("Location: /admin/products");
+   exit;
+});
 
 
 
