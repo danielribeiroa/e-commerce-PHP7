846ffe707b8abde5c87a420585d40652142e0f18
Daniel <danielribeiro.a@outlook.com>
2020-09-16 16:48:56 -0300

    add/remove cart products

--- views-cache/index.90114dbd21b49bd61cb05802c7fce893.rtpl.php
+++ views-cache/index.90114dbd21b49bd61cb05802c7fce893.rtpl.php
@@ -86,12 +86,11 @@
                         <div class="product-carousel">
 
                             <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>
-
                             <div class="single-product">
                                 <div class="product-f-image">
                                     <img src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="">
                                     <div class="product-hover">
-                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i>Comprar</a>
+                                        <a href="/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/add" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i>Comprar</a>
                                         <a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" class="view-details-link"><i class="fa fa-link"></i>Ver detalhes</a>
                                     </div>
                                 </div>
@@ -103,7 +102,6 @@
                                 </div> 
                             </div>
                             <?php } ?>
-
                         </div>
                     </div>
                 </div>
